b71e5433928a43967c7231d347c84fde3f18d99e
DarrenMeidl <dmeidl15@gmail.com>
2024-01-24 11:18:48 +0000

    Removed original code

--- src/CA3_Question1.java
+++ src/CA3_Question1.java
@@ -1,15 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  *  Name: Darren Meidl
  *  Class Group: GD2b
  */
 public class CA3_Question1
 {
+    public static void main(String[] args) {
+        runSimulation();
+    }
     public static void runSimulation()
     {
 
     }
 
-    public static void main(String[] args) {
-        runSimulation();
-    }
+
 }
