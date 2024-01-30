16e8623c274409b02b6ff30dde8b2b5dcfd12337
Darren <dmeidl15@gmail.com>
2024-01-30 18:44:03 +0000

    Q3: create map & scanner

--- src/CA3_Question3.java
+++ src/CA3_Question3.java
@@ -1,4 +1,10 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  *  Name: Darren Meidl
  *  Class Group: GD2b
@@ -6,9 +12,12 @@ import java.io.FileNotFoundException;
 
 public class CA3_Question3
 {
-    public static void readFile(String fileName)
-    {
+    public static void readFile(String fileName) throws FileNotFoundException {
+        Scanner in = new Scanner(new File(fileName)); // Creates a scanner that scans the file we pass in as a string parameter
+        in.useDelimiter("[^A-Za-z0-9_]+"); // Defining delimiter pattern
 
+        Map<String, List<String>> identifiers = new HashMap<>(); // new hasmap called identifiers
+        int lineNumber = 1; // integer for tracking line number
     }
 
     public static void main(String[] args) throws FileNotFoundException {
