c03022b6fc166f7c2e732e56856550784f95734a
DarrenMeidl <dmeidl15@gmail.com>
2024-02-01 15:53:30 +0000

    Q3: Adds the line number to each HashSet during while loops

--- src/CA3_Question3.java
+++ src/CA3_Question3.java
@@ -16,24 +16,26 @@ public class CA3_Question3
 
         while(scanner.hasNextLine()){ // Check if there's a next line
             Scanner in = new Scanner(scanner.nextLine());
-            in.useDelimiter("[^A-Za-z0-9_]+");
+            in.useDelimiter("[^A-Za-z0-9_]+"); // Get rid of anything that does not match these parameters: A-Z, a-z, 0-9 and _
             while(in.hasNext()){ // Goes through each word & adds each to map
-                String token = in.next();
+                String token = in.next(); // Gets next word
 
-                if (map.containsKey(token)){ // assign a line number to the token
+                if (map.containsKey(token)){ // If the map has this key already, assign a line number to the token
                     map.get(token).add(lineNumber);
                 }
-                else{
-                    HashSet<Integer> set = new HashSet<>(); // New hash set
+                else{ // Otherwise, create a new HashSet & add the word (as a key) and the new HashSet (as a value)
+                    HashSet<Integer> set = new HashSet<>(); // New HashSet
+                    set.add(lineNumber); // add the line number to the HashSet
                     map.put(token, set); // add the token & its set to the map
+
                 }
             }
-            lineNumber++;
+            lineNumber++; // Increment the line number
         }
-
+        // Loop that runs through each entry in the map's entry set & prints them
         for (Map.Entry<String, HashSet<Integer>> entry : map.entrySet()) {
-            String str = entry.getKey();
-            HashSet<Integer> hashSet = entry.getValue();
+            String str = entry.getKey(); // the current entry's key
+            HashSet<Integer> hashSet = entry.getValue(); // the current entry's value
             System.out.println("Key: " + str + ", Value: " + hashSet);
         }
     }
