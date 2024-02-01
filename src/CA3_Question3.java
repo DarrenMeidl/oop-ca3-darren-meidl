6085fa7ecb5b2078574ada702dfc28782780874d
DarrenMeidl <dmeidl15@gmail.com>
2024-02-01 15:44:13 +0000

    Q3: Now prints each key & value. Fixed delimiter & new hashset for each entry

--- src/CA3_Question3.java
+++ src/CA3_Question3.java
@@ -1,9 +1,6 @@
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 import java.io.File;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  *  Name: Darren Meidl
@@ -15,12 +12,11 @@ public class CA3_Question3
     public static void readFile(String fileName) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(fileName)); // Scan new file into scanner
         Map<String, HashSet<Integer>> map = new TreeMap<>(); // New tree map
-        HashSet<Integer> set = new HashSet<>(); // New hash set
         int lineNumber = 1;
 
         while(scanner.hasNextLine()){ // Check if there's a next line
             Scanner in = new Scanner(scanner.nextLine());
-            in.useDelimiter("[A-Za-z0-9_]+");
+            in.useDelimiter("[^A-Za-z0-9_]+");
             while(in.hasNext()){ // Goes through each word & adds each to map
                 String token = in.next();
 
@@ -28,11 +24,17 @@ public class CA3_Question3
                     map.get(token).add(lineNumber);
                 }
                 else{
+                    HashSet<Integer> set = new HashSet<>(); // New hash set
                     map.put(token, set); // add the token & its set to the map
                 }
             }
-
             lineNumber++;
+        }
+
+        for (Map.Entry<String, HashSet<Integer>> entry : map.entrySet()) {
+            String str = entry.getKey();
+            HashSet<Integer> hashSet = entry.getValue();
+            System.out.println("Key: " + str + ", Value: " + hashSet);
         }
     }
 
