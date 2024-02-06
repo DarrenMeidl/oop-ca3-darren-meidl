c9e2a48836cb1f86afd0c3d82998fd323fc40994
Darren <dmeidl15@gmail.com>
2024-02-06 17:20:35 +0000

    Q10: Created text file with some example cities

--- src/CA3_Question10.java
+++ src/CA3_Question10.java
@@ -1,3 +1,8 @@
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  *  Name: Darren Meidl
  *  Class Group: GD2b
@@ -6,6 +11,6 @@ public class CA3_Question10
 {
 
     public static void main(String[] args) {
-
+        Map<String, TreeSet<DistanceTo>> connections = new TreeMap<>(); // New tree map
     }
 }
