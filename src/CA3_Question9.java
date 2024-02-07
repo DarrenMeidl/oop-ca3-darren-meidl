0560f5b5db5801e066282f0ecc8f1500b5557404
Darren <dmeidl15@gmail.com>
2024-02-07 18:31:52 +0000

    Q9: Creates outer walls

--- src/CA3_Question9.java
+++ src/CA3_Question9.java
@@ -14,11 +14,32 @@ public class CA3_Question9
 {
     public static int[][]  floodFillStart() {
         int[][] arr = new int[10][10]; // 10 x 10 grid of integers
+        // Set entire grid to 0s, acts as 'blank canvas'
         for (int x = 0; x < 10; x++){ // For each x value
             for(int y = 0; y < 10; y++){ // run through all y values
                 arr[x][y] = 0; // Initialize current cells as open paths
             }
         }
+
+        // Set outer walls in the maze
+        // LEFT WALL
+        for (int i = 0; i < 10; i++) {
+            arr[i][0] = -1; // -1 represents a wall
+        }
+        // RIGHT WALL
+        for (int i = 0; i < 10; i++) {
+            arr[i][9] = -1; // -1 represents a wall
+        }
+        // BOTTOM WALL
+        for (int i = 0; i < 10; i++) {
+            arr[9][i] = -1; // -1 represents a wall
+        }
+        // TOP WALL
+        for (int i = 0; i < 10; i++) {
+            arr[0][i] = -1; // -1 represents a wall
+        }
+
+
         return arr;
     }
     public static void display(int[][] image)
