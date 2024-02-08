74b6907872d540fdedb6da8892ff6ce222e5b990
DarrenMeidl <dmeidl15@gmail.com>
2024-02-08 13:51:41 +0000

    Q5: Created queues, takeoff & land methods

--- src/CA3_Question5.java
+++ src/CA3_Question5.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 /**
  *  Name: Darren Meidl
  *  Class Group: GD2b
@@ -5,6 +8,17 @@
 
 public class CA3_Question5
 {
+    Queue<String> takeoffQueue = new LinkedList<>(); // takeoff queue
+    Queue<String> landQueue = new LinkedList<>(); // landing queue
+    boolean active = true; // boolean to end simulation
+
+    public void takeoff(String plane){
+        takeoffQueue.add(plane);
+    }
+    public void land(PriorityQueue<String> land, String plane){
+        landQueue.add(plane);
+    }
+
 
     public static void main(String[] args)
     {
