fe69ecd89d6d78d074de7f312092504e8b2b0f9b
DarrenMeidl <dmeidl15@gmail.com>
2024-02-09 11:50:28 +0000

    Q6: Sell works, most error messages work

--- src/CA3_Question6.java
+++ src/CA3_Question6.java
@@ -37,24 +37,65 @@ public class CA3_Question6
                 int qty = in.nextInt();
                 System.out.print("\nEnter Price: ");
                 double price = in.nextDouble();
-
+                int totalQuantity = 0;
+                // If there's no shares in the array, then we can't sell anything
                 if (arr.isEmpty()){
                     System.out.println("Error. No shares to sell.");
                 }
-                else if (qty >= arr.getLast().getQuantity() && !arr.isEmpty()){
-                    double profit = (  arr.getLast().getQuantity() * ( price - arr.getLast().getPrice() )  );
-                    qty = qty - arr.getLast().getQuantity();
-                    arr.remove(arr.getLast());
-                    if (qty < arr.getLast().getQuantity()){
-                        arr.getLast().setQuantity( arr.getLast().getQuantity() - qty );
-                        profit = profit + (  arr.getLast().getQuantity() * ( price - arr.getLast().getPrice() )  );
-                        System.out.println("Profit is: "+profit);
-
-                        if(arr.isEmpty()){
-                            System.out.println("ArrayDeque is empty lol");
-                        }
+                // Otherwise, if there is at least 1 or more shares
+                else {
+                    // Calculate the total quantity of all shares we own
+                    for (Block b : arr){
+                        totalQuantity += b.getQuantity();
                     }
+
+
+                    // If our total quantity is bigger or equal to the amount we want to sell, then we can sell
+                    if (totalQuantity >= qty){
+
+
+                        boolean soldTotal = false;
+                        while(!soldTotal){
+                            double profit = 0;
+
+                            if (qty <= 0){
+                                System.out.println("Sold.");
+                                System.out.println("Profit is: "+profit);
+                                soldTotal = true;
+                            }
+
+                            // If the quantity we want to sell is more or equal to the quantity of the last block in the array
+                            else if (qty >= arr.getLast().getQuantity()){
+                                // Calculate the profit from the selling the entire quantity of the last block
+                                profit += profit + (  arr.getLast().getQuantity() * ( price - arr.getLast().getPrice() )  );
+                                qty -= arr.getLast().getQuantity(); // We've sold an entire block's quantity, take it away from the amount we want to sell
+                                totalQuantity -= arr.getLast().getQuantity(); // We've sold an entire block's quantity, take it away from our total quantity
+                                arr.remove(arr.getLast()); // Since we sold the entire last block, remove it from the array
+                                System.out.println("Sold an entire block.");
+                                System.out.println("Current profit is: "+profit);
+                            }
+
+                            // If the amount we want to sell is less than the quantity of the last block in the array
+                            else if (qty < arr.getLast().getQuantity()){
+                                profit += ( (qty * price) - (qty * arr.getLast().getPrice())); // Since we know we can sell the entire selling quantity from 1 block, we use the selling qty in the calculation
+                                arr.getLast().setQuantity( arr.getLast().getQuantity() - qty ); // Take that selling amount from the last block's quantity
+                                totalQuantity -= arr.getLast().getQuantity(); // Take that selling amount from our total quantity
+                                System.out.println("Current profit is: "+profit); // Prints out profit
+                            }
+                        }
+
+
+
+                    }
+                    // If we don't have enough quantity across all blocks to sell the amount we want, we can't sell. e.g. sell 150 but we have only one block with 70 or two blocks with 50
+                    else {
+                        System.out.println("Error. Not enough shares to sell.");
+                    }
+
                 }
+
+
+
 
             }
             else if(command.equals("quit"))
