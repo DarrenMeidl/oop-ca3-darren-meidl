d248337a23037e3afaabc15dc8bb7d2afdc06c4a
Darren <dmeidl15@gmail.com>
2024-02-11 13:27:06 +0000

    Q8: Implemented as much of algorithm as possible from textbook

--- src/CA3_Question8.java
+++ src/CA3_Question8.java
@@ -7,10 +7,38 @@ import java.util.Stack;
 public class CA3_Question8 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Stack<Double> numbers = new Stack<>();
-        Stack<Character> operators = new Stack<>();
+        System.out.println("Enter an arithmetic expression below: ");
+        String equation = in.nextLine();
+        Stack<Integer> numbers = new Stack<>(); // Numbers stack for storing numbers from equation
+        Stack<Character> operators = new Stack<>(); // Operators stack for storing operations from equation
 
-
+        // Runs through all characters in the equation string
+        for (char c : equation.toCharArray()){
+            if (Character.isDigit(c)){ // If the character reads as a number
+                int num = Character.getNumericValue(c); // Convert char to an integer using a built-in method
+                numbers.push(num); // Push the integer to the numbers stack
+            }
+            else if (c == '('){ // If we read a (
+                operators.push(c); // Push the operator to the operators stack
+            }
+            else if (c == '+' || c == '-' || c == '*' || c == '/'){
+                boolean running = true; // Boolean controls while loop
+                while (running){ // While the top of the stack has a higher precedence than op
+                    // Evaluate the top
+                }
+                // push c onto the stack
+            }
+            else if (c == ')'){
+                while(operators.peek() != c){
+                    // Evaluate the top
+                }
+                // Pop the (
+            }
+            // Else if there is no more input
+                while(!operators.isEmpty()){
+                    // Evaluate the top
+                }
+        }
 
 
     }
